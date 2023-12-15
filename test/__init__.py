a8d53dbec808a310c79dc96681edcd7d2be17bca
Rahul Bhargava <rahulbot@gmail.com>
2023-12-15 08:40:52 -0500

    more detailed sort, better paging test

--- test/__init__.py
+++ test/__init__.py
@@ -3,3 +3,4 @@ import os
 INDEX_NAME = "mediacloud_test"
 ELASTICSEARCH_URL = "http://localhost:9200"
 FIXTURES_DIR = os.path.join(os.path.dirname(__file__), "fixtures")
+NUMBER_OF_TEST_STORIES = 5103
