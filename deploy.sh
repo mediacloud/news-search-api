7910478ad427f07477e2bae2175eff4dc69defae
Xavier Frankline <xf.xavierfrank@gmail.com>
2024-05-29 19:00:04 +0300

    fix ES index name prefix

--- deploy.sh
+++ deploy.sh
@@ -158,7 +158,7 @@ DOCKER_COMPOSE_FILE="docker-compose.yml"
 
 export INDEXES="mc_search"
 export ESOPTS='{"timeout": 60, "max_retries": 3}' # 'timeout' parameter is deprecated
-export ELASTICSEARCH_INDEX_NAME_PREFIX="mc_search-*"
+export ELASTICSEARCH_INDEX_NAME_PREFIX="mc_search"
 export TERMFIELDS="article_title,text_content"
 export TERMAGGRS="top,significant,rare"
 export API_PORT
